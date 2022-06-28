9f432ebec0cdba69a28f67d6d71520dfe70e7f0a
db648 <95957492+db648@users.noreply.github.com>
2022-06-28 20:49:29 +0530

    problem 1 done

---
+++ problem1.js
@@ -0,0 +1,41 @@
+const listOfProducts = [{
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan"
+  }
+];
+
+const getUniqueProductCount = () => {
+  let obj={};
+  let c=0;
+  for(let i=0;i<listOfProducts.length;i++){
+    if(obj[listOfProducts[i].productName] === undefined){
+        obj[listOfProducts[i].productName] = 1;
+    }
+    else{
+        obj[listOfProducts[i].productName] += 1;
+    }
+  }
+  console.log(obj)
+}
+getUniqueProductCount()
